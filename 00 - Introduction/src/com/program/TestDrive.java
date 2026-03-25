b2ee01ac3ca964186678a69b058c6f2643cd6412
fidelisray <arjefidelisraymond@gmail.com>
2026-03-25 22:47:46 +0700

    Pengenalan Constructor dan Class Attribute

--- 00 - Introduction/src/com/program/TestDrive.java
+++ 00 - Introduction/src/com/program/TestDrive.java
@@ -1,7 +1,26 @@
 package com.program;
 
+class Mahasiswa {
+
+    String nama_mhs;
+    String NIM_mhs;
+    String prodi_mhs;
+    String fakultas_mhs;
+
+    Mahasiswa(String nama, String NIM, String prodi, String fakultas) {
+        nama_mhs = nama;
+        NIM_mhs = NIM;
+        prodi_mhs = prodi;
+        fakultas_mhs = fakultas;
+    }
+}
+
 public class TestDrive {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Mahasiswa pico = new Mahasiswa("Pico", "18.K1.0062", "Teknik Informatika", "Ilmu Komputer");
+
+        System.out.println(pico.nama_mhs);
+        System.out.println(pico.prodi_mhs);
     }
 }
