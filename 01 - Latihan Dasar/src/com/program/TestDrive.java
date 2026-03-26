8fdabb3397ea2bd9026d3406229c52be288d33d8
fidelisray <arjefidelisraymond@gmail.com>
2026-03-26 18:37:47 +0700

    Membuat class Player, class Weapon, dan class Armor

--- 01 - Latihan Dasar/src/com/program/TestDrive.java
+++ 01 - Latihan Dasar/src/com/program/TestDrive.java
@@ -1,5 +1,42 @@
 package com.program;
 
+class Player {
+    String name;
+    double health;
+    double basicAttack;
+
+    Weapon weapon;
+    Armor armor;
+
+    Player(String name, double health, double basicAttack) {
+        this.name = name;
+        this.health = health;
+        this.basicAttack = basicAttack;
+    }
+}
+
+class Weapon {
+    String name;
+    String weaponType;
+    double damage;
+
+    Weapon(String name, String weaponType, double damage) {
+        this.name = name;
+        this.weaponType = weaponType;
+        this.damage = damage;
+    }
+}
+
+class Armor {
+    String name;
+    double armorHealth;
+
+    Armor(String name, double armorHealth) {
+        this.name = name;
+        this.armorHealth = armorHealth;
+    }
+}
+
 public class TestDrive {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
