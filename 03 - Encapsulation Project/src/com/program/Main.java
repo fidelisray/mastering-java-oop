076914fc765f18137c1c6bee13b8c16e972b2fc9
fidelisray <arjefidelisraymond@gmail.com>
2026-04-09 13:31:59 +0700

    feat: membuat struktur awal class player, class armor, dan class weapon

--- 03 - Encapsulation Project/src/com/program/Main.java
+++ 03 - Encapsulation Project/src/com/program/Main.java
@@ -1,8 +1,51 @@
 package com.program;
 
+class Player {
+    private String name;
+    private int baseHealth;
+    private int baseAttack;
+    private Armor armor;
+    private Weapon weapon;
+
+    public Player(String name, int baseHealth, int baseAttack) {
+        this.name = name;
+        this.baseHealth = baseHealth;
+        this.baseAttack = baseAttack;
+    }
+
+    public void display() {
+        System.out.println("Nama Player : " + this.name);
+        System.out.println("Base Health : " + this.baseHealth);
+        System.out.println("Base Attack : " + this.baseAttack + "\n");
+    }
+}
+
+class Armor {
+    private String armorName;
+    private int health;
+
+    public Armor(String armorName, int health) {
+        this.armorName = armorName;
+        this.health = health;
+    }
+}
+
+class Weapon {
+    private String weaponName;
+    private String weaponType;
+    private int damage;
+
+    public Weapon(String weaponName, String weaponType, int damage) {
+        this.weaponName = weaponName;
+        this.weaponType = weaponType;
+        this.damage = damage;
+    }
+}
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         
+        Player sparky = new Player("Sparky White", 80, 20);
+        sparky.display();
     }
 }
