548477a366c245aea3af85bbae775742e67cf9da
fidelisray <arjefidelisraymond@gmail.com>
2026-04-11 09:29:07 +0700

    feat: Menambahkan behavior equip weapon untuk class player dan logic perubahan Health Player

--- 03 - Encapsulation Project/src/com/program/Main.java
+++ 03 - Encapsulation Project/src/com/program/Main.java
@@ -4,6 +4,9 @@ class Player {
     private String name;
     private int baseHealth;
     private int baseAttack;
+
+    private int accumulateHealth = 0;
+
     private Armor armor;
     private Weapon weapon;
 
@@ -13,20 +16,40 @@ class Player {
         this.baseAttack = baseAttack;
     }
 
-    public void display() {
-        System.out.println("Nama Player : " + this.name);
-        System.out.println("Base Health : " + this.baseHealth);
-        System.out.println("Base Attack : " + this.baseAttack + "\n");
+    public void equipArmor(Armor armor) {
+        if (this.armor == null){
+            this.armor = armor;
+            this.accumulateHealth = this.baseHealth + armor.getAddHealth();
+        }
+    }
+
+    public void info() {
+        System.out.println("Nama Player  : " + this.name);
+        System.out.println("Base Health  : " + this.baseHealth);
+        System.out.println("Base Attack  : " + this.baseAttack);
+        System.out.println("Total Health : " + this.accumulateHealth + "\n");
     }
 }
 
 class Armor {
     private String armorName;
+    private int strength;
     private int health;
 
-    public Armor(String armorName, int health) {
+    public Armor(String armorName, int strength, int health) {
         this.armorName = armorName;
+        this.strength = strength;
         this.health = health;
+    }
+
+    public int getAddHealth() {
+        return this.strength * 2 + this.health;
+    }
+
+    public void info() {
+        System.out.println("Armor Name       : " + this.armorName);
+        System.out.println("Armor Strength   : " + this.strength);
+        System.out.println("Armor Health     : " + this.health + "\n");
     }
 }
 
@@ -46,6 +69,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         
         Player sparky = new Player("Sparky White", 80, 20);
-        sparky.display();
+        sparky.info();
+
+        Armor bodyArmor = new Armor("Body Armor", 2, 100);
+        bodyArmor.info();
+        
+        sparky.equipArmor(bodyArmor);
+        sparky.info();
     }
 }
